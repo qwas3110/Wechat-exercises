65391d1908943302fcaff1f879f35726ca8a5b1c
xiehongwei <283994917@qq.com>
2019-09-02 14:11:01 +0800

    组件跳转页面，传递 传递id

--- components/book/index.js
+++ components/book/index.js
@@ -19,5 +19,13 @@ Component({
    */
   methods: {
 
+    onTap(event) {
+      const bid = this.properties.book.id;
+      
+      wx.navigateTo({
+        url: `/pages/book-detail/book-detail?bid=${bid}`,
+      })
+
+    }
   }
 })
--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,4 +1,4 @@
-// pages/book-detail/book-detail.js
+ // pages/book-detail/book-detail.js
 Page({
 
   /**
@@ -12,7 +12,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const id = options.bid;
+    console.log(id);
   },
 
   /**
