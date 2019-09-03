2f14d2bf8751236bc2f1a21bb6abbec97dcc1662
xiehongwei <283994917@qq.com>
2019-09-03 13:59:28 +0800

    搜索缓存测试，可行

--- components/search/index.js
+++ components/search/index.js
@@ -1,10 +1,20 @@
 // components/search/index.js
+import {
+  KeywordModel
+} from '../../models/keyword.js'
+const keywordModel = new KeywordModel()
+
+
+
+
+
+
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-    
+
   },
 
   /**
@@ -21,5 +31,12 @@ Component({
     onCancel(event) {
       this.triggerEvent('cancel', {}, {})
     },
+
+    onConfirm(event) {
+      // 获取用户搜索关键字
+      const word = event.detail.value;
+      keywordModel.addToHistory(word)
+    }
+
   }
 })
