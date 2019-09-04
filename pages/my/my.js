3b3317e86913a836c63cd24696fe1dcdc683ebe2
xiehongwei <283994917@qq.com>
2019-09-04 11:05:30 +0800

    通过 opensetting 判断用户是否授权

--- pages/my/my.js
+++ pages/my/my.js
@@ -12,16 +12,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.getUserInfo({
-      success: (res) => {
-        console.log(res)
-      }
-    })
+    this.userAuthorized();
   },
 
   //用户授权事件
   onGetUserInfo(event) {
     // console.log(event)
+  },
+
+  // 判断用户是否授权
+  userAuthorized() {
+    wx.getSetting({
+      success: data => {
+        // 如果为ture 代表用户授权过了
+        if (data.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: data => {
+              console.log(data);
+            }
+          })
+        }
+        else {
+
+        }
+      }
+    })
   }
 
  
